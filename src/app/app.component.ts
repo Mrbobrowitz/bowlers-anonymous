896bb27b6477c1025b537eb700f43496f79e39d4
chris.bobrowitz <chris.bobrowitz@gmail.com>
2022-08-03 17:43:35 -0700

    fix: mobile scaling for grid layout thanks https://stackoverflow.com/a/61579196

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {AfterContentInit, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'bowlers-anonymous';
+  columns: number | undefined;
+
+  breakPoints() {
+    switch(true) {
+      case (window.innerWidth <= 480):
+        this.columns = 1;
+        break;
+      case (window.innerWidth > 480 && window.innerWidth <= 640):
+        this.columns = 2;
+        break;
+      case (window.innerWidth > 640 && window.innerWidth <= 992):
+        this.columns = 3;
+        break;
+      default:
+        this.columns = 5;
+    }
+  }
+
+  ngOnInit() {
+    this.breakPoints();
+  }
+
+  onResize(event: any) {
+    this.breakPoints();
+  }
 }
